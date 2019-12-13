1b1aa3d241991047373eb190e5345baa8a0fbd40
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-13 14:16:10 +0530

    Hystrix dashbord

---
+++ src/main/java/com/hystrixDashbore/G2_hystrixDashbore/G2HystrixDashboreApplication.java
@@ -0,0 +1,13 @@
+package com.hystrixDashbore.G2_hystrixDashbore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2HystrixDashboreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2HystrixDashboreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hystrixDashbore/G2_hystrixDashbore/G2HystrixDashboreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrixDashbore.G2_hystrixDashbore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2HystrixDashboreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
