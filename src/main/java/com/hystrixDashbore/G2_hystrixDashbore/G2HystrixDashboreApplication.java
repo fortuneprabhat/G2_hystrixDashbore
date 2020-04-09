f46b9f1f75b35cf82c0d967ac120735cc0902930
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2020-04-10 01:48:31 +0530

    histrix fix

--- src/main/java/com/hystrixDashbore/G2_hystrixDashbore/G2HystrixDashboreApplication.java
+++ src/main/java/com/hystrixDashbore/G2_hystrixDashbore/G2HystrixDashboreApplication.java
@@ -2,8 +2,10 @@ package com.hystrixDashbore.G2_hystrixDashbore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class G2HystrixDashboreApplication {
 
 	public static void main(String[] args) {
